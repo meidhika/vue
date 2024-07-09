0e3594cc7c11ae43f314624518a0deb420da1eb2
Nawa Sapta <128028368+meidhika@users.noreply.github.com>
2024-07-09 10:47:23 +0700

    Create Price.vue

---
+++ Price.vue
@@ -0,0 +1,18 @@
+<template>
+<span>{{this.prefix + Number.parseFloat(value).toFixed(precision)}}</span>
+</template>
+<script>
+export default{
+    props: {
+        value: Number,
+        prefix: {
+            type: String,
+            default: 'Rp'
+        },
+        precision: {
+            type: Number,
+            default: 2
+        }
+    },
+}
+</script>
